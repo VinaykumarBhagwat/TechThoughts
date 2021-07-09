99988fc2a06aea246a3d24403d5cb9324e094413
VinaykumarBhagwat <Vinchi12$>
2021-07-09 16:57:25 -0700

    API work

---
+++ src/Supermarket.API/Startup.cs
@@ -0,0 +1,124 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Supermarket.API.Domain.Repositories;
+using Supermarket.API.Domain.Services;
+using Supermarket.API.Persistence.Contexts;
+using Supermarket.API.Persistence.Repositories;
+using Supermarket.API.Services;
+
+namespace Supermarket.API
+{
+    public class Startup
+    {
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseInMemoryDatabase("supermarket-api-in-memory");
+            });
+
+            services.AddScoped<ICategoryRepository, CategoryRepository>();
+            services.AddScoped<IProductRepository, ProductRepository>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddScoped<ICategoryService, CategoryService>();
+            services.AddScoped<IProductService, ProductService>();
+
+            services.AddAutoMapper();
+
+            //services.AddAuthentication().AddJwtBearer("Token1", options =>
+            //{
+            //    options.TokenValidationParameters = new TokenValidationParameters()
+            //    {
+            //        ValidateIssuer = true,
+            //        ValidIssuer = Issuer,
+            //        ValidateAudience = true,
+            //        ValidAudience = Audience,
+            //        ValidateIssuerSigningKey = true,
+            //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Key)),
+            //    };
+            //    options.Events = new JwtBearerEvents()
+            //    {
+            //        OnMessageReceived = context =>
+            //        {
+            //            var Token = context.Request.Headers["UserCred1"].ToString();
+            //            context.Token = Token;
+            //            return Task.CompletedTask;
+            //        },
+            //    };
+            //})
+            //.AddJwtBearer("Token2", options =>
+            //{
+            //    options.TokenValidationParameters = new TokenValidationParameters()
+            //    {
+            //        ValidateIssuer = true,
+            //        ValidIssuer = Issuer,
+            //        ValidateAudience = true,
+            //        ValidAudience = Audience,
+            //        ValidateIssuerSigningKey = true,
+            //        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Key)),
+            //    };
+            //    options.Events = new JwtBearerEvents()
+            //    {
+            //        OnMessageReceived = context =>
+            //        {
+            //            var Token = context.Request.Headers["UserCred2"].ToString();
+            //            context.Token = Token;
+            //            return Task.CompletedTask;
+            //        },
+            //    };
+            //});
+            //services.AddAuthorization(options =>
+            //{
+            //    options.DefaultPolicy = new AuthorizationPolicyBuilder()
+            //    .RequireAuthenticatedUser()
+            //    .AddAuthenticationSchemes("Token1", "Token2")
+            //    .Build();
+            //});
+            //services.AddAuthorization(options =>
+            //{
+            //    options.AddPolicy("RequireAllSchemes", policy =>
+            //    {
+            //        policy.AddAuthenticationSchemes("Token1");
+            //        policy.AddAuthenticationSchemes("Token2");
+            //        policy.RequireAuthenticatedUser();
+            //        policy.RequireAssertion(context =>
+            //        {
+            //            return context.User.Identities.Count() == 2;
+            //        });
+            //    });
+            //});
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+        }
+    }
+}
